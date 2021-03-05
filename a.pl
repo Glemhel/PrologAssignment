f81b3ce76a6ef6bab5839500b3809e6f4dd33a55
Mikhail Rudakov <ya.rudakov2001@yandex.ru>
2021-03-06 01:23:44 +0300

    fixed backtracking output

--- a.pl
+++ a.pl
@@ -12,9 +12,10 @@ map_height(3).
 map_width(3).
 covid(1, 0).
 covid(0, 1).
-covid(1, 1).
 finish(2, 0).
 man(a).
+
+find_way(Xfinish, Yfinish, Xfinish, Yfinish, _, _, [], _) :- !.
 
 find_way(Xcurrent, Ycurrent, Xfinish, Yfinish, Xmax, Ymax, 
     [[Xcurrent, Ycurrent] | PathTaken], Visited) :-
@@ -26,26 +27,27 @@ find_way(Xcurrent, Ycurrent, Xfinish, Yfinish, Xmax, Ymax,
     Ycurrent =< Ymax,
     Xcurrent >= 0,
     Ycurrent >= 0,
+
     not(covid(Xcurrent, Ycurrent)),
     not(member([Xcurrent, Ycurrent], Visited)),
-    (find_way(Xcurrent, Yup, Xfinish, Yfinish, Xmax, Ymax, 
-        [[Xcurrent, Ycurrent] |PathTaken], [[Xcurrent, Ycurrent] | Visited]);
-    find_way(Xcurrent, Ydown, Xfinish, Yfinish, Xmax, Ymax,
-         [[Xcurrent, Ycurrent] | PathTaken], [[Xcurrent, Ycurrent] | Visited]);
-    find_way(Xup, Yup, Xfinish, Yfinish, Xmax, Ymax,
-         [[Xcurrent, Ycurrent] | PathTaken], [[Xcurrent, Ycurrent] | Visited]);
-    find_way(Xup, Ycurrent, Xfinish, Yfinish, Xmax, Ymax,
-         [[Xcurrent, Ycurrent] | PathTaken], [[Xcurrent, Ycurrent] | Visited]);
-    find_way(Xup, Ydown, Xfinish, Yfinish, Xmax, Ymax,
-         [[Xcurrent, Ycurrent] | PathTaken], [[Xcurrent, Ycurrent] | Visited]);
-    find_way(Xdown, Ycurrent, Xfinish, Yfinish, Xmax, Ymax,
-         [[Xcurrent, Ycurrent] | PathTaken], [[Xcurrent, Ycurrent] | Visited]);
-    find_way(Xdown, Yup, Xfinish, Yfinish, Xmax, Ymax,
-         [[Xcurrent, Ycurrent] | PathTaken], [[Xcurrent, Ycurrent] | Visited]);
-    find_way(Xdown, Ydown, Xfinish, Yfinish, Xmax, Ymax,
-         [[Xcurrent, Ycurrent] | PathTaken], [[Xcurrent, Ycurrent] | Visited]);
-
-    Xcurrent = Xfinish, Ycurrent = Yfinish, print(PathTaken)).
+    (
+     find_way(Xup, Yup, Xfinish, Yfinish, Xmax, Ymax,
+          PathTaken, [[Xcurrent, Ycurrent] | Visited]);
+     find_way(Xcurrent, Yup, Xfinish, Yfinish, Xmax, Ymax, 
+          PathTaken, [[Xcurrent, Ycurrent] | Visited]);
+     find_way(Xup, Ycurrent, Xfinish, Yfinish, Xmax, Ymax,
+          PathTaken, [[Xcurrent, Ycurrent] | Visited]);
+     find_way(Xcurrent, Ydown, Xfinish, Yfinish, Xmax, Ymax,
+          PathTaken, [[Xcurrent, Ycurrent] | Visited]);
+     find_way(Xup, Ydown, Xfinish, Yfinish, Xmax, Ymax,
+          PathTaken, [[Xcurrent, Ycurrent] | Visited]);
+     find_way(Xdown, Ycurrent, Xfinish, Yfinish, Xmax, Ymax,
+          PathTaken, [[Xcurrent, Ycurrent] | Visited]);
+     find_way(Xdown, Yup, Xfinish, Yfinish, Xmax, Ymax,
+          PathTaken, [[Xcurrent, Ycurrent] | Visited]);
+     find_way(Xdown, Ydown, Xfinish, Yfinish, Xmax, Ymax,
+          PathTaken, [[Xcurrent, Ycurrent] | Visited])
+     ).
 
 solve(Xcurrent, Ycurrent, Xfinish, Yfinish, Xmax, Ymax, PathTaken, Visited) :-
     find_way(Xcurrent, Ycurrent, Xfinish, Yfinish, Xmax, Ymax, PathTaken, Visited),
